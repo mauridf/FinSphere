8ab885268d88d449a60d8ca7bd7845c0adfbb026
mauridf <mauridf@gmail.com>
2025-09-19 18:26:45 -0300

    feat(conf):Configurações iniciais

--- FinSphere.API/Program.cs
+++ FinSphere.API/Program.cs
@@ -1,41 +1,34 @@
+using FinSphere.Infrastructure.Configurations;
+using FinSphere.Infrastructure.Data.Context;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers();
+
+// Configure MongoDB
+builder.Services.Configure<MongoDBSettings>(
+    builder.Configuration.GetSection("MongoDBSettings"));
+builder.Services.AddSingleton<MongoDBContext>();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
---
+++ FinSphere.Infrastructure/Data/Context/MongoDBContext.cs
@@ -0,0 +1,18 @@
+﻿using FinSphere.Infrastructure.Configurations;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace FinSphere.Infrastructure.Data.Context;
+
+public class MongoDBContext
+{
+    private readonly IMongoDatabase _database;
+
+    public MongoDBContext(IOptions<MongoDBSettings> settings)
+    {
+        var client = new MongoClient(settings.Value.ConnectionString);
+        _database = client.GetDatabase(settings.Value.DatabaseName);
+    }
+
+    public IMongoDatabase GetDatabase() => _database;
+}
